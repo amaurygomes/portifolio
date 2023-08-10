3f26087baa3f063ed4f2082ce8c4880dd7ad6725
Amaury Gomes Ibanez <amaury_mju@hotmail.com>
2023-08-10 18:18:36 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+async function fetchGitHubUser(username) {
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (!response.ok) {
+        throw new Error('Erro ao obter os dados do usuário');
+      }
+      const userData = await response.json();
+      return userData;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+  
+  // Exemplo de uso da função
+  const username = 'amaurygomes';
+  fetchGitHubUser(username)
+    .then(userData => {
+      if (userData) {
+        const bioParagraph = document.querySelector('.bio');
+        bioParagraph.textContent = userData.bio;
+        const avatarImage = document.querySelector('.img-aside');
+        avatarImage.src = userData.avatar_url;
+    
+
+
+      } else {
+        window.location.href = '404.html';
+      }
+    })
+    .catch(error => {
+      console.error('Erro:', error);
+    });
+  
