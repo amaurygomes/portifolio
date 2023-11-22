1df5ca08f455df76bd1d231b352f5ea2bbd13860
Amaury Gomes Ibanez <amaury_mju@hotmail.com>
2023-11-22 17:58:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Get the sidemenu element
+var sidemenu = document.getElementById("sidemenu");
+
+// To open sidemenu
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+
+// To close sidemenu
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
